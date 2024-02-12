0e4ff98b11fe80aea2f50c2ec9f419607ca6f82b
TusharSanisare <tsanisare@gmail.com>
2024-02-12 17:53:45 +0530

    first

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+function App() {
+  return (
+    <>
+      <Navigation></Navigation>
+      <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
